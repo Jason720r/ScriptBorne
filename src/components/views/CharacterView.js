bb827a44390f2c9f17afc3357542fe71cdc35d8f
Jason Li <jasonli99193@gmail.com>
2023-12-11 11:32:01 -0600

    player work in progress

--- src/components/views/CharacterView.js
+++ src/components/views/CharacterView.js
@@ -2,5 +2,25 @@ import React, { useEffect, useState } from "react";
 
 
 export const CharacterSelect = () => {
-    const [character, SetCharacter ] = useState([]);
+    const [characters, setCharacter ] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8088/players")
+            .then((response) => response.json())
+            .then((PlayerArray) => {
+               setCharacter(PlayerArray); 
+            })
+    }, []);
+
+    return (
+        <>
+        <div className="player_container">
+            <div className="player_view">Characters</div>
+            {characters.map((character) => (
+                <div key={character.id}>
+                    <p className="player_title">{character.name}</p>
+                    </div>
+            ))}
+             </div></>
+    )
 }
