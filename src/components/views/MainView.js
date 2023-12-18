d1088adf4197e4eff312aada88112b7a31cefd9c
Jason Li <jasonli99193@gmail.com>
2023-12-18 14:57:09 -0600

    dialogue struggles

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -8,14 +8,16 @@ import "./Main.css"
 export const Franklin = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(true);
   const [interaction, setInteraction] = useState([]);
+  const [currentNPC, setCurrentNPC] = useState(null);
 
   const handleClosePopup = () => {
     setIsPopupOpen(false);
+    setInteraction(null);
   }
 
   const handleInteraction = () => {
+    setIsPopupOpen(true);
     setInteraction(true);
-
   }
 
   const handleOption1 = () => {
@@ -47,12 +49,13 @@ export const Franklin = () => {
             src={BatBrahm}
             alt="BatBrahm"
             className="npc_two"
-            onClick={() => handleInteraction("Burger Queen")}
+            onClick={() => handleInteraction("BatBrahm")}
           />
           <img
             src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjIiLCJCb2R5IjoiMSIsIkV5ZXMiOiIyOCIsIlNvY2tzIjoiMSIsIkdsb3ZlcyI6IjQiLCJQYW50cyI6IjUiLCJUb3AiOiIxNiIsIkNhcGUiOiI1IiwiSGFpciI6IjQiLCJHbGFzc2VzIjoiMTAiLCJoYWlyVG9uZSI6ImY0ODliNiIsInBhbnRzVG9uZSI6ImVmMmYwMCIsInRvcFRvbmUiOiJlZjJmMDAiLCJzb2Nrc1RvbmUiOiJlZjJmMDAiLCJnbG92ZXNUb25lIjoiNDk2MmY5IiwiZ2xvdmVzVG9uZTIiOiI0OTYyZjkiLCJjYXBlVG9uZSI6IjQ5NjJmOSJ9/1/show.png"
             alt="Burger Queen"
             className="npc_three"
+            onClick={() => handleInteraction("Burger Queen")}
           />
           {/* "Interact" button for Burger Queen */}
           <button onClick={handleInteraction} className="interact_button">
@@ -60,23 +63,37 @@ export const Franklin = () => {
           </button>
         </div>
   
-        {/* Render the pop-up if isPopupOpen is true */}
         {isPopupOpen && (
           <div className="popup_container">
             <div className="popup_content">
-              {/* Image */}
               <img
                 src={JasonImage}
                 alt="Popup Image"
                 className="popup_image"
               />
-              {/* Text */}
               <p className="pop_text">Hello there traveler, welcome to the camp, feel free to introduce yourself to the others before beginning your harrowing journey!</p>
   
               <div className="dialogue_options">
+                {interaction === "start" && (
+                  <>
                 <button onClick={handleOption1} className="dialogue_button">Ok, Thanks!</button>
                 <button onClick={handleOption2} className="dialogue_button">FUck you.</button>
+                </>
+                )}
+
+                {interaction == "option1" && (
+                  <p>Option 1 selected</p>
+                )}
+                 {interaction == "option2" && (
+                  <p>Option 2 selected</p>
+                )}
               </div>
+
+              {interaction !== "option1" && interaction !== "option2" && (
+                <button onClick={handleClosePopup} className="close_button">
+                  Continue
+                </button>
+              )}
             </div>
           </div>
         )}
