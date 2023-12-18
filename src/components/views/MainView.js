436bdcbe880b46e85d10a9f4e8e89d60e535e712
Jason Li <jasonli99193@gmail.com>
2023-12-18 14:26:37 -0600

    styling continues

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -36,69 +36,50 @@ export const Franklin = () => {
   
     return (
       <>
-      <div className="main_container">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/268px-Playstation_logo_colour.svg.png"
-        alt="PlayStation Logo"
-        className="top_right_image"
-    />
-        <img 
-        src={ShrineImage} 
-        alt="shrine"
-        className="script_shrine"
-        >
-          
-        </img>
-
-        <img 
-        src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjciLCJCb2R5IjoiMyIsIkV5ZXMiOiIxNyIsIk1vdXRoIjoiMTQiLCJTaG9lcyI6IjEiLCJHbG92ZXMiOiIxIiwiUGFudHMiOiIxIiwiVG9wIjoiMTMiLCJOZWNrIjoiMyIsIkhhaXIiOiIzMSIsInNraW5Ub25lIjoiZjRkNWMxIiwiZXllc1RvbmUiOiI0ZDdjMWUiLCJoYWlyVG9uZSI6ImYwZWY5MyIsInRvcFRvbmUiOiJmZmViYWIiLCJzaG9lc1RvbmUiOiJlZGVkZWQiLCJjYXBlVG9uZSI6IjUxMTUxNSIsImNhcGVUb25lMiI6ImVjMjQyNCJ9/1/show.png"
-        alt="Granola"
-        className="npc_one"
-        />
-        <img 
-        src={BatBrahm}
-        alt="BatBrahm"
-        className="npc_two"
-        >
-          </img>
-        <img
-        src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjIiLCJCb2R5IjoiMSIsIkV5ZXMiOiIyOCIsIlNvY2tzIjoiMSIsIkdsb3ZlcyI6IjQiLCJQYW50cyI6IjUiLCJUb3AiOiIxNiIsIkNhcGUiOiI1IiwiSGFpciI6IjQiLCJHbGFzc2VzIjoiMTAiLCJoYWlyVG9uZSI6ImY0ODliNiIsInBhbnRzVG9uZSI6ImVmMmYwMCIsInRvcFRvbmUiOiJlZjJmMDAiLCJzb2Nrc1RvbmUiOiJlZjJmMDAiLCJnbG92ZXNUb25lIjoiNDk2MmY5IiwiZ2xvdmVzVG9uZTIiOiI0OTYyZjkiLCJjYXBlVG9uZSI6IjQ5NjJmOSJ9/1/show.png"
-        alt="Burger Queen"
-        className="npc_three"
-        >
-        </img>
-      </div>
-      <button onClick={handleInteraction} className="interact_button">
-        Interact
-      </button>
-      {/* Render the pop-up if isPopupOpen is true */}
-      {isPopupOpen && (
-
-        <div className="popup_container">
-          <div className="popup_content">
-            {/* Image */}
-            <img
-              src={JasonImage}
-              alt="Popup Image"
-              className="popup_image"
-            />
-            {/* Text */}
-            <p className="pop_text">Hello there traveler, welcome to the camp, feel free to introduce yourself to the others before beginning your harrowing journey!</p>
-
-            <div className="dialogue_options">
-              <button onClick={handleOption1}>Nice</button>
-              <button onClick={handleOption2}>Nice 2</button>
-
-            </div>
-
-            <button onClick={handleClosePopup} className="close_button">
-              Continue
-            </button>
+        <div className="main_container">
+          {/* ... (your NPC images) */}
+          <img
+            src={ShrineImage}
+            alt="shrine"
+            className="script_shrine"
+          />
+          <img 
+            src={BatBrahm}
+            alt="BatBrahm"
+            className="npc_two"
+            onClick={() => handleInteraction("Burger Queen")}
+          />
+          <img
+            src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjIiLCJCb2R5IjoiMSIsIkV5ZXMiOiIyOCIsIlNvY2tzIjoiMSIsIkdsb3ZlcyI6IjQiLCJQYW50cyI6IjUiLCJUb3AiOiIxNiIsIkNhcGUiOiI1IiwiSGFpciI6IjQiLCJHbGFzc2VzIjoiMTAiLCJoYWlyVG9uZSI6ImY0ODliNiIsInBhbnRzVG9uZSI6ImVmMmYwMCIsInRvcFRvbmUiOiJlZjJmMDAiLCJzb2Nrc1RvbmUiOiJlZjJmMDAiLCJnbG92ZXNUb25lIjoiNDk2MmY5IiwiZ2xvdmVzVG9uZTIiOiI0OTYyZjkiLCJjYXBlVG9uZSI6IjQ5NjJmOSJ9/1/show.png"
+            alt="Burger Queen"
+            className="npc_three"
+          />
+          {/* "Interact" button for Burger Queen */}
+          <button onClick={handleInteraction} className="interact_button">
+            Interact
+          </button>
+        </div>
+  
+        {/* Render the pop-up if isPopupOpen is true */}
+        {isPopupOpen && (
+          <div className="popup_container">
+            <div className="popup_content">
+              {/* Image */}
+              <img
+                src={JasonImage}
+                alt="Popup Image"
+                className="popup_image"
+              />
+              {/* Text */}
+              <p className="pop_text">Hello there traveler, welcome to the camp, feel free to introduce yourself to the others before beginning your harrowing journey!</p>
+  
+              <div className="dialogue_options">
+                <button onClick={handleOption1} className="dialogue_button">Ok, Thanks!</button>
+                <button onClick={handleOption2} className="dialogue_button">FUck you.</button>
+              </div>
             </div>
           </div>
-        
-      )}
-    </>
-    
+        )}
+      </>
     );
 }
