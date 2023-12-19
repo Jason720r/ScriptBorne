f96470dac0e186f3911c280761167c94963c3d5c
Jason Li <jasonli99193@gmail.com>
2023-12-19 11:31:45 -0600

    item view started

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -3,6 +3,9 @@ import { Franklin } from "./components/views/MainView.js";
 import { CharacterInfo } from "./components/views/CharacterView.js";
 import { AboutUs } from "./components/About.js";
 import { Home } from "./components/ScriptHome.js";
+import { WeaponInfo } from "./components/views/WeaponView.js";
+
+
 export const ApplicationViews =() => {
     return <>
     <Routes>
@@ -10,7 +13,7 @@ export const ApplicationViews =() => {
         <Route path="/main" element={<Franklin />} />
         <Route path="/players" element={<CharacterInfo />} />
         <Route path="/about" element={<AboutUs />} />
-
+        <Route path="/items" element={<WeaponInfo />} />
         
         </Routes></>
     
--- src/components/views/WeaponView.js
+++ src/components/views/WeaponView.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 export const WeaponInfo = () => {
-    const [weapons, setWeapons] = useState([]);
+    const [weapons, setWeapon] = useState([]);
 
 
 
