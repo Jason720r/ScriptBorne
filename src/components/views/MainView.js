3419d903c0b66eef1a7130f4fc67e6e73bdc2e63
Jason Li <jasonli99193@gmail.com>
2023-12-20 13:21:25 -0600

    interaction

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -8,7 +8,10 @@ import "./Main.css"
 
 
 export const Franklin = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(true);
+  const [jasonPopupOpen, setJasonPopupOpen] = useState(false);
+  const [burgerQueenPopupOpen, setBurgerQueenPopupOpen] = useState(false);
+  const [jasonInteraction, setJasonInteraction] = useState("start");
+  const [burgerQueenInteraction, setBurgerQueenInteraction] = useState("start");
   const [interaction, setInteraction] = useState([]);
   const [currentNPC, setCurrentNPC] = useState(null);
   const [popupData, setPopupData] = useState({
@@ -17,25 +20,32 @@ export const Franklin = () => {
     interaction: null,
   })
 
-  const handleClosePopup = () => {
-    setIsPopupOpen(false);
-    setInteraction(null);
+  const handleCloseJasonPopup = () => {
+    setJasonPopupOpen(false);
   }
 
-  const handleInteraction = (npcName) => {
-    setIsPopupOpen(true);
-    setInteraction("start");
-    setCurrentNPC(npcName)
+  const handleCloseBurgerQueenPopup = () => {
+    setBurgerQueenPopupOpen(false);
+  }
+
+  const handleInteractionJason = () => {
+    setJasonPopupOpen(true);
+    setJasonInteraction("start");
+  }
+
+  const handleInteractionBurgerQueen = () => {
+    setBurgerQueenPopupOpen(true);
+    setBurgerQueenInteraction("start")
   }
 
   const handleOption1 = () => {
-    handleClosePopup();
+    handleCloseJasonPopup();
 
   };
   
   const handleOption2 = () => {
 
-    handleClosePopup();
+    handleCloseBurgerQueenPopup();
 
   };
   
@@ -54,21 +64,32 @@ export const Franklin = () => {
             src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjciLCJCb2R5IjoiMyIsIkV5ZXMiOiIxNyIsIk1vdXRoIjoiMTQiLCJTaG9lcyI6IjEiLCJHbG92ZXMiOiIxIiwiUGFudHMiOiIxIiwiVG9wIjoiMTMiLCJOZWNrIjoiMyIsIkhhaXIiOiIzMSIsInNraW5Ub25lIjoiZjRkNWMxIiwiZXllc1RvbmUiOiI0ZDdjMWUiLCJoYWlyVG9uZSI6ImYwZWY5MyIsInRvcFRvbmUiOiJmZmViYWIiLCJzaG9lc1RvbmUiOiJlZGVkZWQiLCJjYXBlVG9uZSI6IjUxMTUxNSIsImNhcGVUb25lMiI6ImVjMjQyNCJ9/1/show.png"
             alt="Nola Granola"
             className="npc_one"
-            onClick={() => handleInteraction("Nola Granola")}
+            // onClick={() => handleInteraction("Nola Granola")}
+          />
+          <img
+            src="http://www.avatarsinpixels.com/minipix/eyJXaW5ncyI6IjMiLCJIYWlyTG93ZXIiOiIyMCIsIkJvZHkiOiIxIiwiRXllcyI6IjQiLCJNb3V0aCI6IjQiLCJVbmRlcndlYXIiOiI0IiwiU2hvZXMiOiI1IiwiR2xvdmVzIjoiMyIsIlBhbnRzIjoiMiIsIkphY2tldCI6IjYiLCJOZWNrIjoiMSIsIkhhaXIiOiI2IiwiSGF0IjoiNCIsImV5ZXNUb25lIjoiYzY5NjFjIiwiZXllc1RvbmUyIjoiYWFjYWQxIiwibWFza1RvbmUiOiIyZTVhNDQiLCJ1bmRlcndlYXJUb25lIjoiMjI0MDA4IiwicGFudHNUb25lIjoiNDQ0NDQ0IiwidG9wVG9uZSI6IjI2ODc1NiIsIndpbmdzVG9uZSI6IjdmNDQ0NCIsIndpbmdzVG9uZTIiOiI4ZTM1MzUiLCJzaG9lc1RvbmUiOiJjNjk2MWMiLCJnbG92ZXNUb25lIjoiYzM4ZDQwIiwiaGF0VG9uZSI6IjM3NTM2ZSIsImhhdFRvbmUyIjoiYzM4ZDQwIiwiamFja2V0VG9uZSI6ImQyZDJkMiIsIm5lY2tUb25lIjoiYzM4ZDQwIn0=/1/show.png"
+            alt="Jason"
+            className="npc_two"
+            onClick={() => handleInteractionJason("Jason")}
           />
           <img
             src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjIiLCJCb2R5IjoiMSIsIkV5ZXMiOiIyOCIsIlNvY2tzIjoiMSIsIkdsb3ZlcyI6IjQiLCJQYW50cyI6IjUiLCJUb3AiOiIxNiIsIkNhcGUiOiI1IiwiSGFpciI6IjQiLCJHbGFzc2VzIjoiMTAiLCJoYWlyVG9uZSI6ImY0ODliNiIsInBhbnRzVG9uZSI6ImVmMmYwMCIsInRvcFRvbmUiOiJlZjJmMDAiLCJzb2Nrc1RvbmUiOiJlZjJmMDAiLCJnbG92ZXNUb25lIjoiNDk2MmY5IiwiZ2xvdmVzVG9uZTIiOiI0OTYyZjkiLCJjYXBlVG9uZSI6IjQ5NjJmOSJ9/1/show.png"
             alt="Burger Queen"
             className="npc_three"
-            onClick={() => handleInteraction("Burger Queen")}
+            onClick={() => handleInteractionBurgerQueen("Burger Queen")}
           />
-          {/* "Interact" button for Burger Queen */}
-          <button onClick={handleInteraction} className="interact_button">
+          <button onClick={handleInteractionJason} className="interact_button">
             Interact
           </button>
+          <button
+          onClick={handleInteractionBurgerQueen}
+          className="interact_button"
+        >
+          Burger Queen
+        </button>
         </div>
   
-        {isPopupOpen && (
+        {jasonPopupOpen && (
           <div className="popup_container">
             <div className="popup_content">
               <img
@@ -85,20 +106,32 @@ export const Franklin = () => {
                 <button onClick={handleOption2} className="dialogue_button">Sure maneee.</button>
                 </>
                 )}
-
-                {popupData.interaction == "option1" && (
-                  <p>Option 1 selected for {popupData.npcName}</p>
-                )}
-                 {popupData.interaction == "option2" && (
-                  <p>Option 2 selected for {popupData.npcName}</p>
-                )}
+                </div>
               </div>
+            </div>
+                )}
 
-              {popupData.interaction !== "option1" && popupData.interaction !== "option2" && (
-                <button onClick={handleClosePopup} className="close_button">
-                  Close {popupData.npcName}'s Pop-up'
-                </button>
-              )}
+                  
+        {burgerQueenPopupOpen && (
+          <div className="popup_container">
+            <div className="popup_content">
+              <img
+                src={BurgerQueen}
+                alt="Popup Image"
+                className="popup_image"
+              />
+              <p className="pop_text">*Stands menacingly</p>
+  
+              <div className="dialogue_options">
+                {burgerQueenInteraction === "start" && (
+                  <>
+                <button onClick={handleOption1} className="dialogue_button">Uhhhh Hi?</button>
+                <button onClick={handleOption2} className="dialogue_button">Leave</button>
+                </>
+                )}
+                
+
+              </div>
             </div>
           </div>
         )}
