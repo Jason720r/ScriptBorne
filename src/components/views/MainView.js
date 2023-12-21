5fac6a36967bf90790d3aa114969c13d6916a31b
Jason Li <jasonli99193@gmail.com>
2023-12-21 13:39:17 -0600

    Granola

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from "react";
 import "./Main.css"
  import JasonImage from './Popup.png';
  import ShrineImage from './Shrine.png';
+ import NolaImage from './Nola.png';
+ import Granola from './Granola.png';
  import BatBrahm from './BatBrahm.png'
  import ShrineVideo from './ShrineVideo.mp4';
  import BurgerQueen from './Burger.png';
 
 
+
 export const Franklin = () => {
   const [jasonPopupOpen, setJasonPopupOpen] = useState(false);
   const [burgerQueenPopupOpen, setBurgerQueenPopupOpen] = useState(false);
+  const [nolaPopupOpen, setNolaPopupOpen] = useState(false);
   const [jasonInteraction, setJasonInteraction] = useState("start");
   const [burgerQueenInteraction, setBurgerQueenInteraction] = useState("start");
+  const [NolaInteraction, setNolaInteraction] = useState("start");
   const [interaction, setInteraction] = useState([]);
   const [currentNPC, setCurrentNPC] = useState(null);
   const [popupData, setPopupData] = useState({
@@ -28,6 +33,10 @@ export const Franklin = () => {
     setBurgerQueenPopupOpen(false);
   }
 
+  const handleCloseNolaPopup = () => {
+    setNolaPopupOpen(false);
+  }
+
   const handleInteractionJason = () => {
     setJasonPopupOpen(true);
     setJasonInteraction("start");
@@ -38,6 +47,11 @@ export const Franklin = () => {
     setBurgerQueenInteraction("start")
   }
 
+  const handleInteractionNola = () => {
+    setNolaPopupOpen(true);
+    setNolaInteraction("start")
+  }
+
   const handleOption1 = () => {
     handleCloseJasonPopup();
 
@@ -46,6 +60,12 @@ export const Franklin = () => {
   const handleOption2 = () => {
 
     handleCloseBurgerQueenPopup();
+
+  };
+
+  const handleOption3 = () => {
+
+    handleCloseNolaPopup();
 
   };
   
@@ -64,7 +84,7 @@ export const Franklin = () => {
             src="http://www.avatarsinpixels.com/minipix/eyJDYXBlQmFjayI6IjciLCJCb2R5IjoiMyIsIkV5ZXMiOiIxNyIsIk1vdXRoIjoiMTQiLCJTaG9lcyI6IjEiLCJHbG92ZXMiOiIxIiwiUGFudHMiOiIxIiwiVG9wIjoiMTMiLCJOZWNrIjoiMyIsIkhhaXIiOiIzMSIsInNraW5Ub25lIjoiZjRkNWMxIiwiZXllc1RvbmUiOiI0ZDdjMWUiLCJoYWlyVG9uZSI6ImYwZWY5MyIsInRvcFRvbmUiOiJmZmViYWIiLCJzaG9lc1RvbmUiOiJlZGVkZWQiLCJjYXBlVG9uZSI6IjUxMTUxNSIsImNhcGVUb25lMiI6ImVjMjQyNCJ9/1/show.png"
             alt="Nola Granola"
             className="npc_one"
-            // onClick={() => handleInteraction("Nola Granola")}
+            onClick={() => handleInteractionNola("Nola Granola")}
           />
           <img
             src="http://www.avatarsinpixels.com/minipix/eyJXaW5ncyI6IjMiLCJIYWlyTG93ZXIiOiIyMCIsIkJvZHkiOiIxIiwiRXllcyI6IjQiLCJNb3V0aCI6IjQiLCJVbmRlcndlYXIiOiI0IiwiU2hvZXMiOiI1IiwiR2xvdmVzIjoiMyIsIlBhbnRzIjoiMiIsIkphY2tldCI6IjYiLCJOZWNrIjoiMSIsIkhhaXIiOiI2IiwiSGF0IjoiNCIsImV5ZXNUb25lIjoiYzY5NjFjIiwiZXllc1RvbmUyIjoiYWFjYWQxIiwibWFza1RvbmUiOiIyZTVhNDQiLCJ1bmRlcndlYXJUb25lIjoiMjI0MDA4IiwicGFudHNUb25lIjoiNDQ0NDQ0IiwidG9wVG9uZSI6IjI2ODc1NiIsIndpbmdzVG9uZSI6IjdmNDQ0NCIsIndpbmdzVG9uZTIiOiI4ZTM1MzUiLCJzaG9lc1RvbmUiOiJjNjk2MWMiLCJnbG92ZXNUb25lIjoiYzM4ZDQwIiwiaGF0VG9uZSI6IjM3NTM2ZSIsImhhdFRvbmUyIjoiYzM4ZDQwIiwiamFja2V0VG9uZSI6ImQyZDJkMiIsIm5lY2tUb25lIjoiYzM4ZDQwIn0=/1/show.png"
@@ -81,11 +101,16 @@ export const Franklin = () => {
           <button onClick={handleInteractionJason} className="interact_jason">
             Jason
           </button>
+
           <button
           onClick={handleInteractionBurgerQueen}
           className="interact_button"
         >
           Burger Queen
+        </button>
+
+        <button onClick={ handleInteractionNola} className="interact_nola">
+          Nola Granola
         </button>
         </div>
   
@@ -104,6 +129,28 @@ export const Franklin = () => {
                   <>
                 <button onClick={handleOption1} className="dialogue_button">Ok, Thanks!</button>
                 <button onClick={handleOption1} className="dialogue_button">Sure maneee.</button>
+                </>
+                )}
+                </div>
+              </div>
+            </div>
+                )}
+
+        {nolaPopupOpen && (
+          <div className="popup_container">
+            <div className="popup_content">
+              <img
+                src={Granola}
+                alt="Popup Image"
+                className="popup_image"
+              />
+              <p className="pop_text">Sup</p>
+  
+              <div className="dialogue_options">
+                {jasonInteraction === "start" && (
+                  <>
+                <button onClick={handleOption3} className="dialogue_button">Uh Hi?</button>
+                <button onClick={handleOption3} className="dialogue_button">Leave</button>
                 </>
                 )}
                 </div>
