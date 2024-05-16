509372852ca9574fd2f0f700b27a913abc0660e4
Jason Li <jasonli99193@gmail.com>
2024-05-15 20:59:31 -0500

    updated view

--- src/components/views/TannerView.js
+++ src/components/views/TannerView.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Tanner from './Tanner.png';
 import Ruins from './Ruins.mp4';
 import ProfilePicture from './pfp.png';
+import Kitty from './kitty.png';
+import Tol from './toooooilet.png';
 import "./Tanner.css"
 
 export const TannerTalk = () => {
@@ -24,17 +26,32 @@ export const TannerTalk = () => {
                     <div className="profile_section">
                         <img src={ProfilePicture} alt="Profile" className="profile_picture" />
                         <div className="user_info">
-                            <h1>Username</h1>
-                            <p>Status: Feeling nostalgic!</p>
+                            <h1>Username:
+                            Enbyriot
+                            </h1>
+                            <p> Work in Progress!</p>
+                        </div>
+                    
+                    </div>
+                    <div className="kitty_container" >
+                    <div className="hello_kitty">
+                            <img src={Kitty} alt="Kitty" className="kitty_logo"/>
+                        </div>
+                        </div>
+                    <div className="content_section">
+                        <h2>About Me</h2>
+                        <p>Imagine you had your own fake MySpace Tumblr page Tanner, imagine.....
+                            I'M STILL WORKING ON IT OKAY
+                        </p>
+                        <div className="hello_toilet">
+                            <img src={Tol} alt="tol" className="tol_logo"/>
                         </div>
                     </div>
                     <div className="content_section">
-                        <h2>About Me</h2>
-                        <p>This is where you can put some information about yourself. Just like the good old MySpace days!</p>
-                    </div>
-                    <div className="content_section">
                         <h2>Interests</h2>
-                        <p>List your interests here. Music, movies, hobbies, etc.</p>
+                        <p className="desc">DAMN LOOK AT THIS TOILET
+                        
+                        DO NOT CLICK IT</p>
                     </div>
                 </div>
             </div>
